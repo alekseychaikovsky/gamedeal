85ccd97e5dcf54c615d0490d6b068c5f3e99b890
Aleksey Chaikovsky <aleksey@mostro.com>
2014-11-20 22:38:38 -0500

    what bad code wow

--- app/services/search_reddit.rb
+++ app/services/search_reddit.rb
@@ -1,5 +1,6 @@
 class SearchReddit
   require 'mechanize'
+  attr_reader :page
 
   def initialize
     @agent = Mechanize.new
@@ -7,6 +8,23 @@ class SearchReddit
   end
 
   def perform
-    @page.search("p.title").text.downcase.include?
+    nodes = @page.search(".thing")
+    nodes.each do |node|
+      node_title = node.css('.title').text.parameterize.gsub("-", " ")
+      Game.find_each do |game|
+        if node_title.include?(game.search_term)
+          node_links = node.css('a').map { |link| link['href'] }
+          node_links.each do |link|
+            comments_link = link if link.include?("/comments/")
+          end
+
+          if comments_link && !(comments_link == game.comments_url)
+            game.comments_url = comments_link
+            game.deal_url = node_links.first
+            game.save
+          end
+        end
+      end
+    end
   end
 end
